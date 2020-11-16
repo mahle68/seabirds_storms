1e7cd8b979fa375dc3b3b917635f419d943ecfa7
mahle68 <mahle68@gmail.com>
2020-11-16 12:24:13 +0100

    updated version

--- SSF_windy_tracks.R
+++ SSF_windy_tracks.R
@@ -26,67 +26,113 @@ library(brinla)
 wgs<-CRS("+proj=longlat +datum=WGS84 +no_defs")
 meters_proj <- CRS("+proj=moll +ellps=WGS84")
 
+NCEP.loxodrome.na <- function (lat1, lat2, lon1, lon2) {
+  deg2rad <- pi/180
+  acot <- function(x) {
+    return(atan(1/x))
+  }
+  lat1 <- deg2rad * lat1
+  lat2 <- deg2rad * lat2
+  lon1 <- deg2rad * lon1
+  lon2 <- deg2rad * lon2
+  deltaLon <- lon2 - lon1
+  pi4 <- pi/4
+  Sig1 <- log(tan(pi4 + lat1/2))
+  Sig2 <- log(tan(pi4 + lat2/2))
+  deltaSig <- Sig2 - Sig1
+  if (deltaLon == 0 && deltaSig > 0) {
+    head <- 0
+  }
+  else if (deltaLon == 0 && deltaSig < 0) {
+    head <- 180
+  }
+  else if (deltaSig == 0 && deltaLon > 0) {
+    head <- 90
+  }
+  else if (deltaSig == 0 && deltaLon < 0) {
+    head <- 270
+  }
+  else if (deltaSig < 0 && deltaLon < 0) {
+    head <- acot(deltaSig/deltaLon) * 180/pi + 180
+  }
+  else if (deltaSig < 0 && deltaLon > 0) {
+    head <- acot(deltaSig/deltaLon) * 180/pi + 180
+  }
+  else if (deltaSig > 0 && deltaLon > 0) {
+    head <- acot(deltaSig/deltaLon) * 180/pi
+  }
+  else if (deltaSig > 0 && deltaLon < 0) {
+    head <- acot(deltaSig/deltaLon) * 180/pi + 360
+  }
+  else {
+    head <-NA}
+  return(head)
+}
+
 #open data: prepared by Sophie
 
-data <- read.csv("/home/enourani/ownCloud/Work/Projects/seabirds_and_storms/data/From_Sophie/DATSETS/WAAL_allGPS_2010-2020_homo_R1h_TrackParam_Wind50kmh.csv") %>% 
-  mutate(date_time = as.POSIXct(strptime(date_time,format = "%Y-%m-%d %H:%M:%S"),tz = "UTC"))
+data <- read.csv("/home/enourani/ownCloud/Work/Projects/seabirds_and_storms/data/From_Sophie/DATSETS/WAAL_allGPS_2010-2020_homo_R1h_TrackParam_Wind50kmh.csv",
+                 na.strings = c("NA",""), fileEncoding="latin1") %>% 
+  mutate(date_time = as.POSIXct(strptime(date_time,format = "%Y-%m-%d %H:%M:%S"),tz = "UTC")) %>% 
+  arrange(TripID,date_time)
+  
 
 
 # STEP 2: prepare alternative steps#####
 
 #create a move object
 
-mv <- move(x = data$Longitude,y = data$Latitude,time = data$date_time, data = data, animal = data$BirdID,proj = wgs)
+mv <- move(x = data$Longitude,y = data$Latitude,time = data$date_time, data = data, animal = data$TripID,proj = wgs)
 
 
 #for each species/flyway, thin the data, burstify, and produce alternative steps
 #create a move list
 
-move_ls<-lapply(split(data,data$group),function(x){
-  x<-as.data.frame(x)
-  mv<-move(x = x$x,y = x$y,time = x$date_time,data = x,animal = x$unique_seg_id,proj = wgs)
-  mv
-})
-move_ls <- move_ls[-2] #remove GFB
+#move_ls<-lapply(split(data,data$group),function(x){
+#  x<-as.data.frame(x)
+#  mv<-move(x = x$x,y = x$y,time = x$date_time,data = x,animal = x$unique_seg_id,proj = wgs)
+#  mv
+#})
+#move_ls <- move_ls[-2] #remove GFB
 
 start_time <- Sys.time()
 
-used_av_ls_1hr <- lapply(move_ls,function(group){ #each group is a species/flyway combo
+#used_av_ls_1hr <- lapply(move_ls,function(group){ #each group is a species/flyway combo
   #group <- mv_OHB #use this as group
-  sp_obj_ls<-lapply(split(group),function(seg){ #sp_obj_ls will have the filtered and bursted segments
+  sp_obj_ls<-lapply(split(mv),function(trip){
     
     #--STEP 1: thin the data to 1-hourly intervals
-    seg_th<-seg%>%
+    trip_th<-trip%>%
       thinTrackTime(interval = as.difftime(1, units='hours'),
-                    tolerance = as.difftime(30, units='mins')) #the unselected bursts are the large gaps between the selected ones
+                    tolerance = as.difftime(15, units='mins')) #the unselected bursts are the large gaps between the selected ones
     #--STEP 2: assign burst IDs (each chunk of track with 1 hour intervals is one burst... longer gaps will divide the brusts) 
-    seg_th$selected <- c(as.character(seg_th@burstId),NA) #assign selected as a variable
-    seg_th$burst_id <-c(1,rep(NA,nrow(seg_th)-1)) #define value for first row
+    trip_th$selected <- c(as.character(trip_th@burstId),NA) #assign selected as a variable
+    trip_th$burst_id <-c(1,rep(NA,nrow(trip_th)-1)) #define value for first row
     
-    if(nrow(seg_th@data) == 1){
-      seg_th@data$burst_id <- seg_th$burst_id
-    } else {for(i in 2:nrow(seg_th@data)){
+    if(nrow(trip_th@data) == 1){ #assign burst_ids
+      trip_th@data$burst_id <- trip_th$burst_id
+    } else {for(i in 2:nrow(trip_th@data)){
       
-      if(i== nrow(seg_th@data)){
-        seg_th@data$burst_id[i]<-NA
+      if(i == nrow(trip_th@data)){
+        trip_th@data$burst_id[i]<-NA
       } else
-        if(seg_th@data[i-1,"selected"] == "selected"){
-          seg_th@data$burst_id[i]<-seg_th@data[i-1,"burst_id"]
+        if(trip_th@data[i-1,"selected"] == "selected"){
+          trip_th@data$burst_id[i]<-trip_th@data[i-1,"burst_id"]
         } else {
-          seg_th@data$burst_id[i]<-seg_th@data[i-1,"burst_id"]+1
+          trip_th@data$burst_id[i]<-trip_th@data[i-1,"burst_id"]+1
         }
     }
     }
     #convert back to a move object (from move burst)
-    seg_th <- as(seg_th,"Move")
+    trip_th <- as(trip_th,"Move")
     
     #--STEP 3: calculate step lengths and turning angles 
     #sl_ and ta_ calculations should be done for each burst. converting to a move burst doesnt make this automatic. so just split manually
-    burst_ls<-split(seg_th,seg_th$burst_id)
+    burst_ls<-split(trip_th,trip_th$burst_id)
     burst_ls<-Filter(function(x) length(x)>=3, burst_ls) #remove bursts with less than 3 observations
     
     burst_ls<-lapply(burst_ls,function(burst){
-      burst$step_length<-c(distance(burst),NA) #
+      burst$step_length<-c(distance(burst),NA) 
       burst$turning_angle<-c(NA,turnAngleGc(burst),NA)
       burst
     })
@@ -96,12 +142,15 @@ used_av_ls_1hr <- lapply(move_ls,function(group){ #each group is a species/flywa
     
     #reassign values
     
-    if(length(bursted_sp) >= 1){
-      bursted_sp$track<-seg@idData$track
-      bursted_sp$group<-seg@idData$group
-    }
+    #if(length(bursted_sp) >= 1){
+    #  bursted_sp$track<-trip@idData$track
+    #  bursted_sp$group<-trip@idData$group
+    #}
     
-    bursted_sp$seg_id<-seg@idData$seg_id 
+    bursted_sp$TripID<-trip@idData$TripID 
+    bursted_sp$BirdID<-trip@idData$BirdID 
+    bursted_sp$sex<-trip@idData$sex 
+    
     bursted_sp
   }) %>% 
     Filter(function(x) length(x) > 1, .) #remove segments with no observation (these have only one obs due to the assignment of segment id)
@@ -123,19 +172,22 @@ used_av_ls_1hr <- lapply(move_ls,function(group){ #each group is a species/flywa
   fit.gamma1 <- fitdist(sl, distr = "gamma", method = "mle")
   
   #plot
-  # X11();par(mfrow=c(1,2))
-  # hist(sl,freq=F,main="",xlab = "Step length (km)")
-  # plot(function(x) dgamma(x, shape = fit.gamma1$estimate[[1]],
-  #                         rate = fit.gamma1$estimate[[2]]), add = TRUE, from = 0.1, to = 150, col = "blue")
-  # 
-  # hist(rad(bursted_df$turning_angle[complete.cases(bursted_df$turning_angle)]),freq=F,main="",xlab="Turning angles (radians)")
-  # plot(function(x) dvonmises(x, mu = mu, kappa = kappa), add = TRUE, from = -3.5, to = 3.5, col = "red")
-  # 
+  jpeg("/home/enourani/ownCloud/Work/Projects/seabirds_and_storms/SSF_process_figures/ta_sl_dist.jpeg")
+   #X11();
+   par(mfrow=c(1,2))
+   hist(sl,freq=F,main="",xlab = "Step length (km)")
+   plot(function(x) dgamma(x, shape = fit.gamma1$estimate[[1]],
+                           rate = fit.gamma1$estimate[[2]]), add = TRUE, from = 0.1, to = 150, col = "blue")
+   
+   hist(rad(bursted_df$turning_angle[complete.cases(bursted_df$turning_angle)]),freq=F,main="",xlab="Turning angles (radians)")
+   plot(function(x) dvonmises(x, mu = mu, kappa = kappa), add = TRUE, from = -3.5, to = 3.5, col = "red")
+   mtext("Example of step length and turning angle distributions", side = 3, outer =T,line = -4, font = 2) 
+   dev.off()
   
   #--STEP 5: produce alternative steps
-  used_av_seg <- lapply(sp_obj_ls, function(seg){ #for each segment
+  used_av_trip <- lapply(sp_obj_ls, function(trip){ #for each trip
     
-    used_av_burst <- lapply(split(seg,seg$burst_id),function(burst){ #for each burst,
+    used_av_burst <- lapply(split(trip,trip$burst_id),function(burst){ #for each burst,
       
       #assign unique step id
       burst$step_id <- 1:nrow(burst)
@@ -146,9 +198,9 @@ used_av_ls_1hr <- lapply(move_ls,function(group){ #each group is a species/flywa
         previous_point<-burst[this_point-1,] #this is the previous point, for calculating turning angle.
         used_point <- burst[this_point+1,] #this is the next point. the observed end-point of the step starting from the current_point
         
-        #randomly generate 69 step lengths and turning angles
-        rta <- as.vector(rvonmises(n = 69, mu = mu, kappa = kappa)) #generate random turning angles with von mises distribution (in radians)
-        rsl<-rgamma(n= 69, shape=fit.gamma1$estimate[[1]], rate= fit.gamma1$estimate[[2]])*1000  #generate random step lengths from the gamma distribution. make sure unit is meters
+        #randomly generate 20 step lengths and turning angles
+        rta <- as.vector(rvonmises(n = 20, mu = mu, kappa = kappa)) #generate random turning angles with von mises distribution (in radians)
+        rsl<-rgamma(n= 20, shape=fit.gamma1$estimate[[1]], rate= fit.gamma1$estimate[[2]])*1000  #generate random step lengths from the gamma distribution. make sure unit is meters
         
         #calculate bearing of previous point
         #prev_bearing<-bearing(previous_point,current_point) #am I allowing negatives?... no, right? then use NCEP.loxodrome
@@ -167,12 +219,12 @@ used_av_ls_1hr <- lapply(move_ls,function(group){ #each group is a species/flywa
         
         #put used and available points together
         df <- used_point@data %>%  
-          slice(rep(row_number(),70)) %>% #paste each row 69 times for the used and alternative steps
-          mutate(x = c(head(x,1),rnd_sp@coords[,1]),
-                 y = c(head(y,1),rnd_sp@coords[,2]),
-                 used = c(1,rep(0,69)))  %>% #one hour after the start point of the step
+          slice(rep(row_number(),21)) %>% #paste each row 20 times for the used and alternative steps
+          mutate(Longitude = c(head(Longitude,1),rnd_sp@coords[,1]),
+                 Latitude = c(head(Latitude,1),rnd_sp@coords[,2]),
+                 used = c(1,rep(0,20)))  %>% #one hour after the start point of the step
           rowwise() %>% 
-          mutate(heading = NCEP.loxodrome.na(lat1=current_point$y,lat2=y,lon1=current_point$x,lon2= x)) %>% 
+          mutate(heading = NCEP.loxodrome.na(lat1=current_point$Latitude,lat2=Latitude,lon1=current_point$Longitude,lon2= Longitude)) %>% 
           as.data.frame()
         
         df
@@ -185,9 +237,36 @@ used_av_ls_1hr <- lapply(move_ls,function(group){ #each group is a species/flywa
     used_av_burst
   }) %>% 
     reduce(rbind)
-  used_av_seg
-})
+  #used_av_trip
+#})
 Sys.time() - start_time
 
+#rename x and y columns and assign NA to repeated values that don't make sense
+used_alt_trip <- used_av_trip %>% 
+  dplyr::select(-c("Latitude","Longitude", "accumulated_dist", "turningAngle_deg", "turningAngle_rad", "selected", "speed_kmh", "dtBefore_sec")) %>% 
+  rename(Latitude = y, Longitude = x) 
+  # rowwise %>% 
+  # mutate_at(c("turning_angle", "step_length", "angleBirdWind", "windAzimuth", "windSpeed_kmh", "windDirection", "windSpeed_ms", "v_wind_interp", 
+  #             "u_wind_interp", "v_wind", "t", "travelled_distance_km", "DistColo"),
+  #           ~ ifelse(used == 0, NA, .))
 
+used_alt_trip[used_alt_trip$used == 0, c("turning_angle", "step_length", "angleBirdWind", "windAzimuth", "windSpeed_kmh", "windDirection", "windSpeed_ms", "v_wind_interp", 
+                    "u_wind_interp", "v_wind", "t", "travelled_distance_km", "DistColo")] <- NA
+  
+write.csv(used_alt_trip,"/home/enourani/ownCloud/Work/Projects/seabirds_and_storms/data/From_Sophie/DATSETS/WAAL_allGPS_2010-2020_homo_R1h_TrackParam_Wind50kmh_alt_steps.csv")
+save(used_alt_trip, file = "/home/enourani/ownCloud/Work/Projects/seabirds_and_storms/data/From_Sophie/DATSETS/WAAL_allGPS_2010-2020_homo_R1h_TrackParam_Wind50kmh_alt_steps.RData")
+
+#plot
+jpeg("/home/enourani/ownCloud/Work/Projects/seabirds_and_storms/SSF_process_figures/alt_step_eg.jpeg")
+#X11()
+maps::map("world",xlim = c(49,57), ylim = c(-49,-44.9),fil = TRUE,col = "ivory") #flyway
+title("Example of used and available steps")
+legend("bottomleft", legend = c("track","previous point", "start point","alternative end points", "used end point"),
+      col = c("grey", "yellow4", "firebrick1","orange", "firebrick4"), pch = 16, bty = "n")
+points(burst,col = "grey", pch = 16, cex = 1)
+points(previous_point,col = "yellow4", pch = 16, cex = 2)
+points(current_point,col = "firebrick1", pch = 16, cex = 2)
+points(rnd_sp, col = "orange", pch = 16, cex = 1)
+points(used_point, col = "firebrick4", pch = 16, cex = 2)
+dev.off()
 
