74c524d99daa369389820902f9e2dd8ef64a2eac
mahle68 <mahle68@gmail.com>
2021-08-17 13:36:59 +0200

    update

--- PCoA_seabirds.R
+++ PCoA_seabirds.R
@@ -125,59 +125,59 @@ wind_br <- ann %>%
 
 save(wind_br, file = "R_files/wind_br_18spp.RData")
 
-## STEP 2: summarize timing of breeding ####
-#species-colony-specific
-# (median of all timestamps)
-files_ls <- list.files("/home/enourani/ownCloud/Work/Projects/seabirds_and_storms/annotation/all_spp_for_pcoa/", pattern = ".csv",recursive = T, full.names = T)
+## STEP 3: linear modeling ####
 
-timing <- lapply(files_ls, read.csv, stringsAsFactors = F) %>% 
-  reduce(full_join) %>% 
-  drop_na(ECMWF.ERA5.SL.Sea.Surface.Temperature) %>%   #remove points over land
-  mutate(timestamp,timestamp = as.POSIXct(strptime(timestamp,format = "%Y-%m-%d %H:%M:%S"),tz = "UTC")) %>%
-  mutate(month = month(timestamp),
-         yday = yday(timestamp)) %>% 
-  group_by(sci_name, colony.name) %>% 
-  summarise(median_breeding_m = median(month),
-            median_breeding_yday = median(yday))
+#------------------ prep input data 
+load("R_files/wind_br_18spp.RData")
+load("R_files/data_morph_PCs.RData")
 
 #add wind and morphology variables
-lm_input <- timing %>% 
+lm_input <- wind_br %>% 
   left_join(species[,c(1,2)], by = c("sci_name" = "scientific.name")) %>% 
   full_join(data_pca, by = "species") %>% 
-  dplyr::select(c(1,5,8,2,14,15,3,4,16:20,9:13,6,7)) 
+  dplyr::select(c(1,12,15,2,8:11,16:20,3:7,13,14)) %>% #reorder the columns
+  ungroup()
 
 save(lm_input, file = "R_files/lm_input_20spp_col.RData")
 
-#also save as csv and send to Emily
-write.csv(lm_input[,-c(19,20)], "R_files/wind_data.csv")
+#save as csv and send to Emily
+write.csv(as.data.frame(lm_input[,-c(19,20)]), "R_files/wind_data.csv")
 
+#------------------ z-transform and correlation
+lm_input_z <- lm_input %>% 
+  mutate_at(c("colony.long","colony.lat", "wing.loading..Nm.2.", "wing.span..m.", "wing.area..m2.", "PC1", "PC2"),
+            list(z = ~as.numeric(scale(.)))) %>% 
+  mutate(breeding_ch = as.character(median_breeding_m))
 
-## STEP 4: LM and GAM ####
-
-#on the one-row-per-species dataset
-load("R_files/data_w_PCs.RData") #data_pca
-
-data_pca[data_pca$flight.type == "gliding-soaring / shearing", "flight.type"] <- "dynamic soaring"
-
-#LM
-
-
-
-
-#z-transform and correlation
-data_sc <- data_pca %>% 
-  mutate_at(c("body.mass..kg.","wing.span..m.","wing.area..m2.","wing.loading..Nm.2.","aspect.ratio", "colony.lat", "colony.long"),
-          list(z = ~as.numeric(scale(.)))) 
-
-data_sc %>% 
-dplyr::select(c("body.mass..kg.","wing.span..m.","wing.area..m2.","wing.loading..Nm.2.","aspect.ratio")) %>% 
+lm_input %>% 
+  dplyr::select(c("colony.long","colony.lat","median_breeding_m","median_breeding_yday", "wing.loading..Nm.2.", "wing.area..m2.", "wing.span..m.", "PC1", "PC2")) %>% 
   correlate() %>% 
   stretch() %>% 
-  filter(abs(r) > 0.6) #PC1 and PC2 are correlated
+  filter(abs(r) > 0.6) #PC1 and PC2 are correlated; median month and yday are correlated
+
+#------------------ modeling
+
+m_location <-  lm(max_wind ~ colony.long + colony.lat, data = lm_input) #adj Rsq = 0.4352 
+m_lat <-  lm(max_wind ~ colony.lat, data = lm_input) #0.384 
+m_morph <- lm(max_wind ~ PC1, data = lm_input) #0.171
+m_morph2 <- lm(max_wind ~ wing.loading..Nm.2., data = lm_input) #0.2898
+m_morph3 <- lm(max_wind ~ wing.loading..Nm.2._z + wing.area..m2._z, data = lm_input_z) #0.3096
+m_loc_morph <- lm(max_wind ~ colony.long_z + colony.lat_z + wing.loading..Nm.2._z, data = lm_input_z) #0.4978 
 
 
-m1 <- lm(max_wind ~ colony.long + colony.lat + wing.loading..Nm.2._z, data = data_sc) #wing loading and aspect ratio are correlated
-m1b <- lm(max_wind ~ colony.lat + wing.loading..Nm.2._z, data = data_sc) #wing loading and aspect ratio are correlated
+
+m1 <- lm(max_wind ~ colony.long + colony.lat + PC1 + median_breeding_m, data = lm_input) 
+
+m2 <- lm(max_wind ~ colony.long_z + colony.lat_z + PC1_z + breeding_ch, data = lm_input_z) 
+
+
+m3 <- m1 <- lm(max_wind ~ colony.lat * PC1 + median_breeding_m, data = lm_input) #adj Rsq = 0.6175 #this model is wrong because timing of breeding is assumed continuous
+
+m4 <- m1 <- lm(max_wind ~ colony.lat * PC1 , data = lm_input) #adj Rsq = 0.4032
+
+m5 <- m1 <- lm(max_wind ~ colony.lat * PC1 + breeding_ch, data = lm_input_z) #adj Rsq = 0.4656 
+
+m6 <- lm(max_wind ~ colony.lat + wing.loading..Nm.2., data = lm_input) #adj Rsq = 0.4793 
 
 
 m2 <- lm(max_wind ~ colony.long + colony.lat + PC1, data = data_sc) 
@@ -207,90 +207,20 @@ boxplot(data$max_wind ~ data$flight.type)
 library(mgcv)
 all_data$yday <- yday(all_data$timestamp)
 
-
 g1 <- gamm(wind_speed_ms ~ s(location.lat, location.long, k = 100) +
              s(yday, bs = "cc") +
              avg_mass + flight.type , method = "REML", data = all_data) #, 
 #weights = varPower(form = ~ lat))
 
 
-g2 <- gam(max_wind ~ s(colony.lat_z, colony.long_z),
-          data = data_sc)
+g2 <- gam(max_wind ~ s(colony.lat, colony.long) +
+            s(median_breeding_yday, bs = "cc"),
+          data = lm_input)
 
+g2 <- gam(max_wind ~ s(colony.lat, colony.long, k = 100) +
+            wing.loading..Nm.2.,
+          data = lm_input)
 
-###############################
-## STEP 2: clustering #### 
-#do i need this step?
-
-#https://uc-r.github.io/kmeans_clustering
-
-load("R_files/morph_wind_20spp_col.RData") #data
-
-#data_z <- scale(data[-12,c(8:12)])
-#data_z <- scale(data[-12,c(8,11,12)]) #only mass, wing loading and aspect ratio
-
-#Only include variables related to wing shape: wing loading, aspect ratio, wing span
-#data_z <- scale(data[-12,c(9,11,12)]) #remove great shearwater. no wing loading info
-data_z <- scale(data[-12,c(11,12)])
-#data_z <- scale(data[-12,c(8,11,12)])
-
-
-#optimal number of clusters
-fviz_nbclust(data_z, kmeans, method = "wss") #elbow method
-fviz_nbclust(data_z, kmeans, method = "silhouette") #silhouette method
-
-
-distance <- get_dist(data_z, method = "manhattan")
-
-fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
-
-
-k2 <- kmeans(data_z, centers = 2, nstart = 25)
-str(k2)
-
-fviz_cluster(k2, data = data_z, ggtheme = theme_bw())
-
-
-k3 <- kmeans(data_z, centers = 3, nstart = 25)
-str(k3)
-
-fviz_cluster(k3, data = data_z, ggtheme = theme_bw())
-
-k4 <- kmeans(data_z, centers = 4, nstart = 25)
-str(k4)
-
-fviz_cluster(k4, data = data_z, ggtheme = theme_bw())
-
-k5 <- kmeans(data_z, centers = 5, nstart = 25)
-str(k5)
-
-fviz_cluster(k5, data = data_z, ggtheme = theme_bw())
-
-
-
-k6 <- kmeans(data_z, centers = 6, nstart = 25)
-str(k6)
-
-fviz_cluster(k6, data = data_z, ggtheme = theme_bw())
-
-
-
-save(k4, file = "R_files/clustering_4k.R")
-save(k5, file = "R_files/clustering_5k.R")
-
-
-
-
-#clustering for wind and geography
-data_w <- scale(data[-12,c(2:4)]) #wind
-fviz_nbclust(data_w, kmeans, method = "silhouette") #silhouette method
-
-
-dist_w <- get_dist(data_w, method = "manhattan")
-
-
-k5_w <- kmeans(data_w, centers = 2, nstart = 25)
-str(k5_w)
-
-fviz_cluster(k5_w, data = data_z, ggtheme = theme_bw())
-
+g2 <- gam(max_wind ~ 
+            s(wing.loading..Nm.2.),
+          data = lm_input)
